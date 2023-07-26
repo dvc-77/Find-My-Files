0874c0439920bacd5ad1d04e2a59e479dbd8f965
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-07-26 03:40:35 +0000

    Remove unnecessary

--- move.py
+++ move.py
@@ -7,7 +7,7 @@ def move(src_file: str, dest_folder: str, base_path: str):
 
     destination_path = os.path.join(base_path, dest_folder, file_name)
 
-    print(f"{destination_path} {file_name}")
+    # print(f"{destination_path} {file_name}")
 
     try:
         shutil.move(src_file, destination_path)
@@ -15,10 +15,3 @@ def move(src_file: str, dest_folder: str, base_path: str):
     except Exception as e:
         print("Error moving {}: {}".format(src_file, str(e)))
         return False
-
-
-move(
-    "c:/Users/Anon/Downloads/Test/Testme.txt",
-    "Documents",
-    "c:/Users/Anon/Downloads/Test",
-)
