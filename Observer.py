450b59ca9ffeb77046108a8895028dacc6757c85
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-07-26 03:48:01 +0000

    Complete organizer

--- Observer.py
+++ Observer.py
@@ -3,16 +3,51 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from functools import lru_cache
+from Move import move
 
 
 # Dictionary for extensions
 All_file_extensions = {
-    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp'],
-    'Documents': ['.doc', '.docx', '.pdf', '.txt', '.rtf', '.odt', '.xls', '.xlsx', '.ppt', '.pptx'],
-    'Videos': ['.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm', '.3gp', '.mpeg', '.mpg'],
-    'Audio': ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a'],
-    'Compressed': ['.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.tar.gz', '.tar.bz2', '.tar.xz']
+    "Images": [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".svg", ".webp"],
+    "Documents": [
+        ".doc",
+        ".docx",
+        ".pdf",
+        ".txt",
+        ".rtf",
+        ".odt",
+        ".xls",
+        ".xlsx",
+        ".ppt",
+        ".pptx",
+    ],
+    "Videos": [
+        ".mp4",
+        ".avi",
+        ".mkv",
+        ".mov",
+        ".wmv",
+        ".flv",
+        ".webm",
+        ".3gp",
+        ".mpeg",
+        ".mpg",
+    ],
+    "Audio": [".mp3", ".wav", ".ogg", ".flac", ".aac", ".wma", ".m4a"],
+    "Compressed": [
+        ".zip",
+        ".rar",
+        ".7z",
+        ".tar",
+        ".gz",
+        ".bz2",
+        ".xz",
+        ".tar.gz",
+        ".tar.bz2",
+        ".tar.xz",
+    ],
 }
+
 
 @lru_cache(maxsize=12)
 def compareExtension(file_extension: str):
@@ -20,22 +55,23 @@ def compareExtension(file_extension: str):
         if file_extension in value:
             return key
 
+
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if event.is_directory:
             return
 
         file_extension = os.path.splitext(event.src_path)[1]
-        
-        # Getting Folder to Move file to 
+
+        # Getting Folder to Move file to
         to_folder = compareExtension(file_extension)
-        
+
         # Left move function from one of the branches
-        
-        
+        move(event.src_path, to_folder, folder_to_watch)
+
         # This function will be called when a new file is created in the monitored folder.
         print(f"New file added: {event.src_path} extension: {file_extension}")
-        
+        print("File moved")
 
 
 if __name__ == "__main__":
@@ -46,6 +82,8 @@ if __name__ == "__main__":
     observer = Observer()
     observer.schedule(event_handler, path=folder_to_watch, recursive=False)
     observer.start()
+
+    print("Active")
 
     try:
         while True:
