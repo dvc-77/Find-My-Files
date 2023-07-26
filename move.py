913ba2ad0fade408b416417be829b2c5aa907966
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-07-26 03:39:42 +0000

    Changed how base_path is obtained

--- move.py
+++ move.py
@@ -2,15 +2,12 @@ import os
 import shutil
 
 
-# Input he path you want to monitor
-base_path = "c:/Users/Anon/Downloads/Test"
-
-
-def move(src_file, dest_folder):
+def move(src_file: str, dest_folder: str, base_path: str):
     file_name = os.path.basename(src_file)
+
     destination_path = os.path.join(base_path, dest_folder, file_name)
 
-    # print(f"{destination_path} {file_name}")
+    print(f"{destination_path} {file_name}")
 
     try:
         shutil.move(src_file, destination_path)
@@ -18,3 +15,10 @@ def move(src_file, dest_folder):
     except Exception as e:
         print("Error moving {}: {}".format(src_file, str(e)))
         return False
+
+
+move(
+    "c:/Users/Anon/Downloads/Test/Testme.txt",
+    "Documents",
+    "c:/Users/Anon/Downloads/Test",
+)
