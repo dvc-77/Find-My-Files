fd7079e6c879f2be8b556ff115f87c2a4973568e
An0n-xen <fyfybaisieamaoh6@gmail.com>
2023-08-01 00:10:28 +0000

    Fixes #8

--- Observer.py
+++ Observer.py
@@ -1,9 +1,48 @@
 import time
-import os
+import os, platform, winreg
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from functools import lru_cache
 from Move import move
+
+
+def add_to_startup() -> None:
+    script_path = os.path.abspath(__file__)
+
+    key = winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\CurrentVersion\Run",
+        0,
+        winreg.KEY_READ,
+    )
+
+    try:
+        existing_value, _ = winreg.QueryValueEx(key, "Observe")
+        if existing_value == script_path:
+            print("Script is already in the startup applications.")
+            return
+    except WindowsError:
+        pass
+
+    n_key = winreg.OpenKey(
+        winreg.HKEY_CURRENT_USER,
+        r"Software\Microsoft\Windows\CurrentVersion\Run",
+        0,
+        winreg.KEY_SET_VALUE,
+    )
+
+    winreg.SetValueEx(n_key, "Observe", 0, winreg.REG_SZ, script_path)
+
+    winreg.CloseKey(key)
+
+    print("[+] Operation complete [+]")
+
+
+def MakeStartApp() -> None:
+    sys_platform = platform.system()
+
+    if sys_platform == "Windows":
+        add_to_startup()
 
 
 # Dictionary for extensions
@@ -50,7 +89,7 @@ All_file_extensions = {
 
 
 @lru_cache(maxsize=12)
-def compareExtension(file_extension: str):
+def compareExtension(file_extension: str) -> str:
     for key, value in All_file_extensions.items():
         if file_extension in value:
             return key
@@ -58,7 +97,7 @@ def compareExtension(file_extension: str):
     return "Unknown"
 
 
-def OrganizeExisting():
+def OrganizeExisting() -> None:
     files = [
         os.path.join(folder_to_watch, x)
         for x in os.listdir(folder_to_watch)
@@ -98,6 +137,10 @@ if __name__ == "__main__":
     # Specify the folder you want to monitor here.
     folder_to_watch = "c:/Users/Anon/Downloads"
 
+    # Add file to registry
+    MakeStartApp()
+
+    # Organizing folder
     OrganizeExisting()
 
     event_handler = MyHandler()
